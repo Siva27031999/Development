e4814cfbaa52e582cd05be46ffdbe32775e2c5a9
Sivagurunathan Vijayakamaraj <siva.infy@outlook.com>
2025-09-14 02:50:40 +0530

    swagger ui

--- src/main/java/com/siva/portal/PortalApplication.java
+++ src/main/java/com/siva/portal/PortalApplication.java
@@ -1,4 +1,4 @@
-package com.siva.portal.application;
+package com.siva.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

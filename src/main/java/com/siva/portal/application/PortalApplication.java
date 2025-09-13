d351d83c52a6457ec2621fe8fa2dd8520aba336e
Sivagurunathan Vijayakamaraj <siva.infy@outlook.com>
2025-09-14 02:38:15 +0530

    Controller commit

--- src/main/java/com/siva/portal/application/PortalApplication.java
+++ src/main/java/com/siva/portal/application/PortalApplication.java
@@ -1,4 +1,4 @@
-package com.siva.portal.portal;
+package com.siva.portal.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

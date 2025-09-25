41bb2a326cfb5aa1f0bc68bc804d30a0da270d4b
Sivagurunathan Vijayakamaraj <siva.infy@outlook.com>
2025-09-26 03:06:14 +0530

    Udeploy api call

---
+++ src/main/java/com/siva/portal/integration/udeploy/UdeployClientException.java
@@ -0,0 +1,15 @@
+﻿package com.siva.portal.integration.udeploy;
+
+/**
+ * Runtime exception thrown when UDeploy calls or processing fail.
+ */
+public class UdeployClientException extends RuntimeException {
+
+    public UdeployClientException(String message) {
+        super(message);
+    }
+
+    public UdeployClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
